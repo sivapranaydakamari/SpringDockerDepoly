ac504773cb5525a9a7a34abe83d0431b11edcb49
sivapranaydakamari <sivapranay1423@gmail.com>
2026-03-11 20:15:27 +0530

    Added the spring REST API code file

---
+++ src/main/java/com/example/demo/CseController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CseController {
+	@GetMapping("/add")
+	public int cseAdd(@RequestParam int a, @RequestParam int b) {
+		return a + b;
+	}
+}
